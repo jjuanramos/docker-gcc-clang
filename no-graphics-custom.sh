f0209b037446fd17df34dcb48c432fc13af43f4c
jjuanramos <jjuanramos91@gmail.com>
2024-09-11 16:41:26 +0200

    version without graphics

---
+++ no-graphics-custom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+currentfolder=${PWD}
+cd ~
+echo "Which is the folder of your host computer you want to find in the container?:"
+printf "${HOME}"/
+read -e origin 
+
+echo DLC_WORK_VOLUME_ORIGIN=\""${HOME}"/$origin\"\ > $currentfolder/srcs/.env
+
+cd $currentfolder
+
+make down
+docker volume rm srcs_ubuntu_gcc_clang
+make
+docker exec -it ubuntu /bin/zsh
